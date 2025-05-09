4148983adb9ef52c7cd72a1f8a2bfcddd03d1c2d
Bilal Fawad Khan <47599282+bilalfawadkhan@users.noreply.github.com>
2025-05-09 21:43:36 +0100

    Hover colour and new about_page

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <img/>
+        <h1 className="text-center borel-regular text-[clamp(3.5rem,10vw,7rem)] m-24"> In Progress</h1>
+        <h2 className="text-center borel-regular text-[clamp(3.5rem,10vw,7rem)] m-24"> In Progress</h2>
+        <p>I am a Front end enginneer focusing on the full stack development to become a well rounder</p>
+    </div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -17,28 +17,28 @@ export default function Home() {
     <h2 className="pt-10 text-center borel-regular lg:text-2xl">I'm passionate about developing ideas into reality</h2>  
   </div>  
     <div className="grid grid-cols-1 p-8 gap-x-14 gap-y-52 lg:grid-cols-12 mt-28">
-        <div className="group col-span-12 lg:col-span-4 lg:col-start-1 overflow-hidden bg-customPink h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPinkDark hover:shadow-amber-600 hover:-translate-y-1">
+        <div className="group col-span-12 lg:col-span-4 lg:col-start-1 overflow-hidden bg-customPink h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPinkDark hover:shadow-customPink  hover:-translate-y-1">
             <div className="flex flex-col items-end h-full ">
                 <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                 <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
                 <img src="https://picsum.photos/id/870/200?grayscale&blur=2" className="w-11/12 transition duration-500 ease-in-out grow rounded-r-xl rounded-t-xl group-hover:translate-y-3 z-1"/>
             </div>
         </div>
-        <div className="group col-span-12 lg:col-span-8 overflow-hidden bg-customYellow h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customYellowDark hover:shadow-amber-600 hover:-translate-y-1">
+        <div className="group col-span-12 lg:col-span-8 overflow-hidden bg-customYellow h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customYellowDark hover:shadow-customYellow hover:-translate-y-1">
             <div className="flex flex-col items-end h-full ">
                 <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                 <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
                 <img src="https://picsum.photos/id/870/200?grayscale&blur=2" className="w-11/12 transition duration-500 ease-in-out grow rounded-r-xl rounded-t-xl group-hover:translate-y-3 z-1"/>
             </div>
         </div>
-        <div className="group col-span-12 lg:col-span-7 overflow-hidden bg-customTeal h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customTealDark hover:shadow-amber-600 hover:-translate-y-1">
+        <div className="group col-span-12 lg:col-span-7 overflow-hidden bg-customTeal h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customTealDark hover:shadow-customTeal hover:-translate-y-1">
             <div className="flex flex-col items-end h-full ">
                 <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                 <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
                 <img src="https://picsum.photos/id/870/200?grayscale&blur=2" className="w-11/12 transition duration-500 ease-in-out grow rounded-r-xl rounded-t-xl group-hover:translate-y-3 z-1"/>
             </div>
         </div>
-        <div className="group col-span-12 lg:col-span-5 overflow-hidden bg-customPurple h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPurpleDark hover:shadow-amber-600 hover:-translate-y-1">
+        <div className="group col-span-12 lg:col-span-5 overflow-hidden bg-customPurple h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPurpleDark hover:shadow-customPurple hover:-translate-y-1">
             <div className="flex flex-col items-end h-full ">
                 <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                 <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
@@ -49,14 +49,14 @@ export default function Home() {
     <div>
         <h1 className="text-center borel-regular text-[clamp(3.5rem,10vw,7rem)] m-24"> In Progress</h1>
         <div className="grid grid-cols-1 p-8 gap-x-14 gap-y-52 lg:grid-cols-12 mt-28">
-            <div className="group col-span-12 lg:col-span-6 lg:col-start-1 overflow-hidden bg-customPink h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPinkDark hover:shadow-amber-600 hover:-translate-y-1">
+            <div className="group col-span-12 lg:col-span-6 lg:col-start-1 overflow-hidden bg-customPink h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customPinkDark hover:shadow-customPink hover:-translate-y-1">
                 <div className="flex flex-col items-end h-full ">
                     <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                     <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
                     <img src="https://picsum.photos/id/870/200?grayscale&blur=2" className="w-11/12 transition duration-500 ease-in-out grow rounded-r-xl rounded-t-xl group-hover:translate-y-3 z-1"/>
                 </div>
             </div>
-            <div className="group col-span-12 lg:col-span-6 overflow-hidden bg-customYellow h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customYellowDark hover:shadow-amber-600 hover:-translate-y-1">
+            <div className="group col-span-12 lg:col-span-6 overflow-hidden bg-customYellow h-72 lg:h-[32rem] rounded-[3rem] transition duration-500 ease-in-out hover:shadow-xl hover:bg-customYellowDark hover:shadow-customYellow hover:-translate-y-1">
                 <div className="flex flex-col items-end h-full ">
                     <h4 className="pt-4 pr-6 text-lg">Little Description</h4>
                     <h2 className="pb-4 pr-6 text-3xl">Project Name</h2>
